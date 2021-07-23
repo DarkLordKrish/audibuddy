8ddb362821841786d24c4c2dc1e2d2fcfe7cae6f
DarkLordKrish <krishichpal5@gmail.com>
2021-07-23 17:37:07 +0530

    now it works

--- speechtotext_mod.py
+++ speechtotext_mod.py
@@ -7,8 +7,12 @@ r = sr.Recognizer()
 def Speechtotext():
   with sr.Microphone() as source:
   #    read audio from mic
-    audio_data = r.listen(source,phrase_time_limit=5)
-    print("Recognising......")
-    text = r.recognize_google(audio_data)
-    print(text)
+    try:
+      audio_data = r.listen(source,phrase_time_limit=5)
+      print("Recognising......")
+      text = r.recognize_google(audio_data)
+      print(text)
+    except:
+      print("Sorry i couldnt recognise")
+      
 
