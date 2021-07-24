f36c4bf6cf1bab4189b5cbea090a3a448bd7b8a3
DarkLordKrish <krishichpal5@gmail.com>
2021-07-24 11:12:45 +0530

    exit_update

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import speechtotext_mod
 import texttospeech_mod
+def main_code():
+    feature_choice = input("What feature do u want? \n speech to text \n text to speech \n :- ")
 
-feature_choice = input("What feature do u want? \n speech to text \n text to speech \n :- ")
+    # exit = input("exit?(y/n): ")
+    if feature_choice == "speech to text":
+        speechtotext_mod.Speechtotext()
+    elif feature_choice == "text to speech":
+        texttospeech_mod.texttospeech()
+    else:
+        print("Invalid response")
+        main_code()
+main_code()
+exit = input("exit?(y/n): ")
+while exit != "y":
+    main_code()
+    exit = input("exit?(y/n): ")
+quit()
 
-if feature_choice == "speech to text":
-    speechtotext_mod.Speechtotext() 
-elif feature_choice == "text to speech":
-    texttospeech_mod.texttospeech()
-else:
-    print("Invalid responce")
+
+
 
 
