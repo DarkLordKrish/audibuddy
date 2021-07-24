6e4f94dd6051a99c9c75534163bc91e3520905e1
DarkLordKrish <krishichpal5@gmail.com>
2021-07-24 09:30:56 +0530

    f

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import speechtotext_mod
 import texttospeech_mod
-speechtotext_mod.Speechtotext()
-texttospeech_mod.texttospeech()
+
+feature_choice = input("What feature do u want?\nspeech to text\ntext to speech")
+
+if feature_choice == "speech to text":
+    speechtotext_mod.Speechtotext() 
+elif feature_choice == "text to speech":
+    texttospeech_mod.texttospeech()
+else:
+    print("Invalid responce")
+
+
