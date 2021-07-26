0aec3f4b62ec81870bfab1f6cda9dc6b5f29cc70
DarkLordKrish <krishichpal5@gmail.com>
2021-07-26 16:50:55 +0530

    texttospeech_android_stable

--- kivy_main.py
+++ kivy_main.py
@@ -1,14 +1,45 @@
-import speechtotext_mod
-import texttospeech_mod
+# import speechtotext_mod
+# import texttospeech_mod
+import pyaudio
+import speech_recognition as sr
+import pyttsx3
+from playsound import playsound
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+class MyGrid(GridLayout):
+    engine = pyttsx3.init()
+    def __init__(self,**kwargs):
+        super(MyGrid,self).__init__(**kwargs)
+        self.inside = GridLayout()
+        self.inside.rows = 3
+        self.rows = 1
+        self.inside.add_widget(Label(text="Type your text: "))
+        self.name = TextInput()
+        self.inside.add_widget(self.name)
+        self.convert = Button(text = "Convert to speech")
+        self.convert.bind(on_press = self.Texttospeech)
+        self.inside.add_widget(self.convert)
+        self.add_widget(self.inside)
+    def Texttospeech(self,instance):
+        name = self.name.text
+        self.engine.say(name)
+        self.engine.runAndWait()
+        print(name)
+        self.name.text = ""
+        
+        
+    
+        
 
+    
+    
 class Audibuddy(App):
     def build(self):
-        return Label(text = "Does this work")
+        return MyGrid()
 if __name__  ==  "__main__":
     Audibuddy().run()
--- texttospeech_mod.py
+++ texttospeech_mod.py
@@ -1,11 +1,9 @@
 import pyaudio
 import speech_recognition as sr
-import gtts
 import pyttsx3
 from playsound import playsound
 engine = pyttsx3.init()
-def texttospeech():
+def texttospeech(data):
   engine = pyttsx3.init()
-  text_data = input("Type something: ")
-  engine.say(text_data)
+  engine.say(data)
   engine.runAndWait()
